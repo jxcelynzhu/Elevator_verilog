e0ed7d0cdedfe484d2b776edefa4a281381fef00
jxcelynzhu <114823228+jxcelynzhu@users.noreply.github.com>
2024-10-18 07:48:15 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -101,16 +101,16 @@ module elevator_state_machine (
     always @(posedge clk or negedge rst_n) begin
         if (~rst_n) begin
           current_state <= IDLE_STATE;
-          current_floor <= 1;
+          current_floor <= 0;
           delay <= 0;
     end else begin
       current_state <= next_state; //Update the current state
 
       if (delay == DELAY_COUNT) begin
         delay <= 0; //Reset delay
-        if (current_state == MOVING_UP && current_floor < 4) 
+          if (current_state == MOVING_UP && current_floor < 4'd4) 
           current_floor <= current_floor + 1;
-        else if (current_state == MOVING_DOWN && current_floor > 1) 
+          else if (current_state == MOVING_DOWN && current_floor > 4'd1) 
           current_floor <= current_floor - 1;
       end else 
          delay <= delay + 1;
