ec6a4b46a40a982db23ef6581f6417b785c78f9f
jxcelynzhu <114823228+jxcelynzhu@users.noreply.github.com>
2024-10-17 23:30:58 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -23,7 +23,10 @@ module tt_um_example (
   wire [3:0] floor;
  
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, 1'b0};
+  //wire _unused = &{ena, 1'b0};
+
+  wire _unused = &{ena, ui_in[7:4], uio_in[7:0], 1'b0};
+
   elevator_state_machine em (
     .clk(clk),
     .rst_n(rst_n),
@@ -42,7 +45,7 @@ endmodule
 
 module elevator_state_machine (
   input clk, // Clock signal
-  input rst_n, // Reset signal 
+  input rst_n, // Reset signal inverted
   input wire [3:0] requested_floor,
   output reg [3:0] current_floor,
   output reg idle_display 
@@ -87,8 +90,8 @@ module elevator_state_machine (
     
   
   // Sequential logic 
-  always @(posedge clk or posedge rst_n) begin
-    if (rst_n) begin
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
           current_state <= IDLE_STATE;
           current_floor <= 0;
           delay <= 0;
@@ -116,24 +119,6 @@ module segment7(
   input wire [3:0] floor, // 4 bit input to display digits < 10
   output reg [6:0] segment // 7 bit output for 7-segment display
 );
- 
-  /*
-  always @(*) begin
-    case (floor)
-      0: segment = 7'b1000000; 
-      1: segment = 7'b1111001;
-      2: segment = 7'b0100100;
-      3: segment = 7'b0110000;
-      4: segment = 7'b0011001;
-      5: segment = 7'b0010010;
-      6: segment = 7'b0000010;
-      7: segment = 7'b1111000;
-      8: segment = 7'b0000000;
-      9: segment = 7'b0010000;
-      default: segment = 7'b1111111;
-    endcase
-  end*/
-  
   
     always @(*) begin
     case (floor)
