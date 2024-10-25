458ec2e17a9cb72cf463ef7441ee73a01a18be63
jxcelynzhu <114823228+jxcelynzhu@users.noreply.github.com>
2024-10-25 14:39:20 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -86,26 +86,26 @@ module elevator_state_machine (
     endcase
   end
   
-  // Sequential logic 
-    always @(posedge clk or negedge rst_n) begin
-        if (~rst_n) begin
-          current_state <= IDLE_STATE;
-          current_floor <= 0;
-          delay <= 0;
-    end else begin
-     current_state <= next_state; //Update the current state
-        
-      //Update the current_floor
-      if (delay == DELAY_COUNT) begin
-        delay <= 0; //Reset delay
-        if (current_state == MOVING_UP) 
-          current_floor <= current_floor + 1;
-        else if (current_state == MOVING_DOWN) 
-          current_floor <= current_floor - 1;
-      end else 
-         delay <= delay + 1;  //increment delay counter
-    end 
-  end
+    // Sequential logic 
+        always @(posedge clk or negedge rst_n) begin
+            if ( ~rst_n ) begin
+              current_state <= IDLE_STATE;
+              current_floor <= 0;
+              delay <= 0;
+        end else begin
+          current_state <= next_state; //Update the current state
+            
+          //Update the current_floor
+          if (delay == DELAY_COUNT) begin
+            delay <= 0; //Reset delay
+            if (current_state == MOVING_UP) 
+              current_floor <= current_floor + 1;
+            else if (current_state == MOVING_DOWN) 
+              current_floor <= current_floor - 1;
+          end else 
+             delay <= delay + 1;  //increment delay counter
+        end
+      end
 endmodule
 
 
